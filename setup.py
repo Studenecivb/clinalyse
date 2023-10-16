990c077a9cde11f321c17734dad87784ec097398
Nina <ninahaladova@gmail.com>
2023-10-16 16:06:50 +0200

    changed setup.py for upload to pypi

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
    name='clinalyse',
    version='1.0.0',
    description='Module for analysing multi-locus clines',
-   author='Stuart JE Baird & Nina Haladova',
-   author_email='stuartj.e.baird@gmail.com',
+   author='Nina Haladova & Stuart JE Baird',
+   author_email='ninahaladova@gmail.com',
    packages=['clinalyse'],
    install_requires=['matplotlib', 'numpy', 'pandas', 'psutil', 'scipy'],
 )
